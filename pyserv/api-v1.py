f03ca572ab55859871564be95e42d56a9191db06
tum0xa <timofey.samodurov@mail.ru>
2019-11-30 16:27:05 +0500

    Added root 'black-friday'

--- pyserv/api-v1.py
+++ pyserv/api-v1.py
@@ -5,6 +5,7 @@ url = "http://chelhack.deletestaging.com/goods"
 
 app = FastAPI()
 
+
 @app.get("/black-friday")
 def read_root():
     raw_json = get_json_with_all_goods(url)
@@ -19,12 +20,14 @@ def read_categories():
     status = raw_json['status']
     return {'categories': categories}
 
+
 @app.get("/categories/{category_id}")
 def read_goods_of_category(category_id: int):
     raw_json = get_json_with_all_goods(url)
     categories = get_categories(raw_json)
     status = raw_json['status']
     return {'categories': categories}
+
 
 @app.get('/status')
 def status():
@@ -47,4 +50,3 @@ def get_categories(raw_json):
         if category not in categories:
             categories.append(category)
     return categories
-
