eb6046d5d005c3fa2c715a76227ecf95c1b25c0e
tum0xa <timofey.samodurov@mail.ru>
2019-11-30 15:03:38 +0500

    Added get_categories func

--- api-v1.py
+++ api-v1.py
@@ -6,21 +6,22 @@ url = "http://chelhack.deletestaging.com/goods"
 
 app = FastAPI()
 
-# class Category(BaseModel):
-#     # id: int
-#     name: str
-
-
 @app.get("/categories")
 def read_categories():
     raw_json = get_json_with_all_goods()
     print(raw_json)
     # return {"categories": categories}
 
+
 def get_json_with_all_goods(url):
     return requests.get(url).json()
 
 
-
-
-
+def get_categories(raw_json):
+    categories = []
+    goods = raw_json['data']
+    for good in goods:
+        category = good['category']
+        if category not in categories:
+            categories.append(category)
+    return categories
